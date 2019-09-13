8057cd06aab6a3047c122acbe209e1fcfd28cfad
Bohdan Chornocondratenko <bogdan.chornocondratenko@gmail.com>
2019-09-13 17:20:37 +0300

    fix scroll and nav

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,34 @@
 //= component/jquery.js
-//= component/smooth-scroll.js
 // = component/maskedinput.js
+
+/*
+    Smooth scroll
+*/
+
+let headerHeight = $('.header').outerHeight();
+
+// Add smooth scrolling to all links
+$("a").on('click', function(event) {
+    event.preventDefault();
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+        // Prevent default anchor click behavior
+
+        // Store hash
+        var hash = this.hash;
+
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top - headerHeight
+        }, 800, function(){
+
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            // window.location.hash = hash;
+        });
+    } // End if
+
+});
 
 /*
     Nav with active class on scroll
@@ -13,7 +41,7 @@ let lastId;
 let cur = [];
 
 window.addEventListener("scroll", event => {
-  let fromTop = window.scrollY;
+  let fromTop = window.scrollY + headerHeight;
 
   mainNavLinks.forEach(link => {
     let section = document.querySelector(link.hash);
