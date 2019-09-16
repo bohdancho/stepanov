d7a0cd40bff9958612526ab4e79cd3c4fc12979f
Bohdan Chornocondratenko <bogdan.chornocondratenko@gmail.com>
2019-09-16 16:41:49 +0300

    fix footer

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,4 @@
 //= component/jquery.js
-// = component/maskedinput.js
 
 /*
     Smooth scroll
@@ -9,10 +8,10 @@ let headerHeight = $('.header').outerHeight();
 
 // Add smooth scrolling to all links
 $("a").on('click', function(event) {
-    event.preventDefault();
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
         // Prevent default anchor click behavior
+        event.preventDefault();
 
         // Store hash
         var hash = this.hash;
